edae011ef8eb96348c419d9ef5d607e51237e8af
Sammy Shear <sammyshear1@gmail.com>
2025-09-11 11:41:54 -0400

    fixed variable names

--- internal/api.go
+++ internal/api.go
@@ -30,17 +30,17 @@ var basicMap = map[string]string{
 }
 
 const (
-	VOWELS                   = "([aeiouəɪʌ])"
-	VOWELS_FULL              = "([aeiouəɪʌ][\\x{0306}\\x{0303}\\x{031E}\\x{031D}\\x{0320}\\x{031F}]?)"
-	CONSONANTS               = "([ʔbcdfghjklmnprstvwxzʃʒ(tʃ)ʂ̻ʐ̻(tʂ̻)][\\x{207F}\\x{02B2}\\x{02E0}]?)"
-	HUSHED_CONSONANTS        = "([csz])"
-	NASAL_RELEASE_CONSONANTS = "([bdfgkptv])"
-	UNVOICING_CONSONANTS     = "([bdgjlmnrvwz])"
-	VOICING_CONSONANTS       = "([cfhkpstx])"
-	VELARIZING_CONSONANTS    = "([bdgjlmnrvwfhkptx])"
-	PALATALIZING_CONSONANTS  = "([bdgjlmnrvwfhkptxsczʃʒ(tʃ)ʂ̻ʐ̻(tʂ̻)])"
-	NON_PHONETIC_NOTATION    = "%s(?<text>[A-Za-z\\d\\s]*)(QP)"
-	BASE_NOTATION            = "(%s)"
+	Vowels                 = "([aeiouəɪʌ])"
+	VowelsFull             = "([aeiouəɪʌ][\\x{0306}\\x{0303}\\x{031E}\\x{031D}\\x{0320}\\x{031F}]?)"
+	Consonants             = "([ʔbcdfghjklmnprstvwxzʃʒ(tʃ)ʂ̻ʐ̻(tʂ̻)][\\x{207F}\\x{02B2}\\x{02E0}]?)"
+	HushedConsonants       = "([csz])"
+	NasalReleaseConsonants = "([bdfgkptv])"
+	UnvoicingConsonants    = "([bdgjlmnrvwz])"
+	VoicingConsonants      = "([cfhkpstx])"
+	VelarizingConsonants   = "([bdgjlmnrvwfhkptx])"
+	PalatalizingConsonants = "([bdgjlmnrvwfhkptxsczʃʒ(tʃ)ʂ̻ʐ̻(tʂ̻)])"
+	NonPhoneticNotation    = "%s(?<text>[A-Za-z\\d\\s]*)(QP)"
+	BaseNotation           = "(%s)"
 )
 
 var hushedMap = map[string]string{
@@ -56,34 +56,34 @@ var semiHushedMap = map[string]string{
 }
 
 var vowelKeys = []RegexpKey{
-	{regexp.MustCompile(VOWELS + "(94)"), "%c\u0306"},
-	{regexp.MustCompile(VOWELS + "(\\+)"), "%c\u0303"},
-	{regexp.MustCompile(VOWELS + "(4)"), "%c\u031E"},
-	{regexp.MustCompile(VOWELS + "(5)"), "%c\u031D"},
-	{regexp.MustCompile(VOWELS + "(7)"), "%c\u0320"},
-	{regexp.MustCompile(VOWELS + "(8)"), "%c\u031F"},
-	{regexp.MustCompile(VOWELS_FULL + "(95)"), "%c."},
-	{regexp.MustCompile(VOWELS_FULL + "(,)(,)"), "\u02C8%c"},
-	{regexp.MustCompile(VOWELS_FULL + "(,)"), "\u02CC%c"},
+	{regexp.MustCompile(Vowels + "(94)"), "%c\u0306"},
+	{regexp.MustCompile(Vowels + "(\\+)"), "%c\u0303"},
+	{regexp.MustCompile(Vowels + "(4)"), "%c\u031E"},
+	{regexp.MustCompile(Vowels + "(5)"), "%c\u031D"},
+	{regexp.MustCompile(Vowels + "(7)"), "%c\u0320"},
+	{regexp.MustCompile(Vowels + "(8)"), "%c\u031F"},
+	{regexp.MustCompile(VowelsFull + "(95)"), "%c."},
+	{regexp.MustCompile(VowelsFull + "(,)(,)"), "\u02C8%c"},
+	{regexp.MustCompile(VowelsFull + "(,)"), "\u02CC%c"},
 }
 
 var consKeys = []RegexpKey{
-	{regexp.MustCompile(HUSHED_CONSONANTS + "(\\+)"), "hushed"},
-	{regexp.MustCompile(HUSHED_CONSONANTS + "(7)"), "semi-hushed"},
-	{regexp.MustCompile(UNVOICING_CONSONANTS + "(2)"), "%c\u0325"},
-	{regexp.MustCompile(VOICING_CONSONANTS + "(2)"), "%c\u032C"},
-	{regexp.MustCompile(VELARIZING_CONSONANTS + "(7)"), "%c\u02E0"},
-	{regexp.MustCompile(PALATALIZING_CONSONANTS + "(8)"), "%c\u02B2"},
-	{regexp.MustCompile(NASAL_RELEASE_CONSONANTS + "(\\+)"), "%c\u207F"},
-	{regexp.MustCompile(CONSONANTS + "(,)"), "%c\u0329"},
+	{regexp.MustCompile(HushedConsonants + "(\\+)"), "hushed"},
+	{regexp.MustCompile(HushedConsonants + "(7)"), "semi-hushed"},
+	{regexp.MustCompile(UnvoicingConsonants + "(2)"), "%c\u0325"},
+	{regexp.MustCompile(VoicingConsonants + "(2)"), "%c\u032C"},
+	{regexp.MustCompile(VelarizingConsonants + "(7)"), "%c\u02E0"},
+	{regexp.MustCompile(PalatalizingConsonants + "(8)"), "%c\u02B2"},
+	{regexp.MustCompile(NasalReleaseConsonants + "(\\+)"), "%c\u207F"},
+	{regexp.MustCompile(Consonants + "(,)"), "%c\u0329"},
 }
 
 // keys for notations
 
 var notKeys = []RegexpKey{
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "0")), "question not asked"},
-	{regexp.MustCompile(fmt.Sprintf(NON_PHONETIC_NOTATION, `(?:^|[^A-Za-z\d])(\+ BUT)`)), " yes but: %s"},
-	{regexp.MustCompile(fmt.Sprintf(NON_PHONETIC_NOTATION, `(?:^|[^A-Za-z\d])(\- BUT)`)), " no but: %s"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "0")), "question not asked"},
+	{regexp.MustCompile(fmt.Sprintf(NonPhoneticNotation, `(?:^|[^A-Za-z\d])(\+ BUT)`)), " yes but: %s"},
+	{regexp.MustCompile(fmt.Sprintf(NonPhoneticNotation, `(?:^|[^A-Za-z\d])(\- BUT)`)), " no but: %s"},
 	{regexp.MustCompile(`(?:^|[^A-Za-z\d])(\+\$)`), " yes\\, but doubtful"},
 	{regexp.MustCompile(`(?:^|[^A-Za-z\d])(\-\$)`), " no\\, but doubtful"},
 	{regexp.MustCompile(`(?:^|[^A-Za-z\d])(\+)`), " yes"},
@@ -110,73 +110,73 @@ var notKeys = []RegexpKey{
 	{regexp.MustCompile("(OVRPMP)"), " overprompted (editor's comment)"},
 	{regexp.MustCompile("(SC)"), ";"},
 	{regexp.MustCompile("(XX)"), " (sic)"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(ADJ)")), "adjective"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(AMER)")), "american yiddish development"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(ANG)")), "anglicism"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(AP)")), "applies to"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, `Q(\-AP)`)), "does not apply to"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(BF)")), "yes, fragment in book"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(B)")), "yes, text in protocol book"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(CF)")), "interviewer's comment: compare"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(DG)")), "disgust"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(EDS)")), "editor's query"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(EDN)")), "editor disagrees"},
-	{regexp.MustCompile(fmt.Sprintf(NON_PHONETIC_NOTATION, "Q(ED)")), "editor's comments follow: %s"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(ELSW)")), "elsewhere"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(EM)")), "emphatic"},
-	{regexp.MustCompile(fmt.Sprintf(NON_PHONETIC_NOTATION, "Q(ENG)")), "explanation in english: %s"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(ETC)")), "etc\\:"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(ET)")), "etymology supplied by informant"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(FR)")), "yes, fragment on tape"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, `Q(F\/Y)`)), "response of wife or other female bystander"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(GERM)")), "Informant’s statement that word is not Yiddish but German"},
-	{regexp.MustCompile(fmt.Sprintf(NON_PHONETIC_NOTATION, "Q(GLE)")), "informant's explanation in English: "},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(GLY)")), "informant's explanation in Yiddish: "},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(GL)")), "gloss"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(HUM)")), "amusing"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(HUNG)")), "informant's statement that word is not Ydidish but Hungarian"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(H)")), "heard but not used"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(INF)")), "infinitive"},
-	{regexp.MustCompile(fmt.Sprintf(NON_PHONETIC_NOTATION, "Q(I GL)")), "Interviewer's Summary: %s"},
-	{regexp.MustCompile(fmt.Sprintf(NON_PHONETIC_NOTATION, "Q(I)")), "interviewer's comments follow: %s"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(K)")), "known"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, `Q(\-K)`)), "unknown"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(LAT)")), "not on tape"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(LIT)")), "literary"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(MEMX)")), "informant's surpise at own recollection"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, `Q(M\/Y)`)), "response by husband or other male bystander"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(NEX)")), "did not exist"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(NN)")), "notVeryNew"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(NOUN)")), "noun"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(NP)")), "unprompted answer to prompted question"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(NT)")), "not on tape"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(OF)")), "oldfashioned"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(OOF)")), "Very Oldfashioned"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(OTW)")), "Otherwise"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(POL)")), "Informant's statement that word is not Yiddish but Polish"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(Q)")), "Check answer on tape"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(RR)")), "very rare"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(RUM)")), "Informant's statement that word is not Yiddish but Rumanian"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(RUS)")), "Informant's statement that word is not Yiddish but Russian"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(RTR)")), "rather"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(R)")), "rare"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(SMT)")), "notSometimes"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(SYN)")), "synonym"},
-	{regexp.MustCompile(fmt.Sprintf(NON_PHONETIC_NOTATION, "Q(S)")), "said by: %s"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(TA)")), "tape audited"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(TF)")), "yes, fragment on tape"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(T)")), "yes, text on tape"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, `Q(\-T)`)), "text not on tape"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(UU)")), "VeryCommon"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(U)")), "Usual"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, `Q(\-U)`)), "Unusual"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(VB)")), "Verb"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(VL)")), "Vulgar"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(V)")), "Proverb"},
-	{regexp.MustCompile(fmt.Sprintf(NON_PHONETIC_NOTATION, "Q(W)")), "used by: %s"},
-	{regexp.MustCompile(fmt.Sprintf(NON_PHONETIC_NOTATION, `Q(\-W)`)), "not used by: %s"},
-	{regexp.MustCompile(fmt.Sprintf(NON_PHONETIC_NOTATION, "Q(YID)")), "Informant's explanation in Yiddish but not necessarily verbatim or phoenetically accurate: %s"},
-	{regexp.MustCompile(fmt.Sprintf(BASE_NOTATION, "Q(ZZ)")), "interviewer's comment: not elicitable"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(ADJ)")), "adjective"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(AMER)")), "american yiddish development"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(ANG)")), "anglicism"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(AP)")), "applies to"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, `Q(\-AP)`)), "does not apply to"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(BF)")), "yes, fragment in book"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(B)")), "yes, text in protocol book"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(CF)")), "interviewer's comment: compare"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(DG)")), "disgust"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(EDS)")), "editor's query"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(EDN)")), "editor disagrees"},
+	{regexp.MustCompile(fmt.Sprintf(NonPhoneticNotation, "Q(ED)")), "editor's comments follow: %s"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(ELSW)")), "elsewhere"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(EM)")), "emphatic"},
+	{regexp.MustCompile(fmt.Sprintf(NonPhoneticNotation, "Q(ENG)")), "explanation in english: %s"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(ETC)")), "etc\\:"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(ET)")), "etymology supplied by informant"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(FR)")), "yes, fragment on tape"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, `Q(F\/Y)`)), "response of wife or other female bystander"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(GERM)")), "Informant’s statement that word is not Yiddish but German"},
+	{regexp.MustCompile(fmt.Sprintf(NonPhoneticNotation, "Q(GLE)")), "informant's explanation in English: "},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(GLY)")), "informant's explanation in Yiddish: "},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(GL)")), "gloss"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(HUM)")), "amusing"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(HUNG)")), "informant's statement that word is not Ydidish but Hungarian"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(H)")), "heard but not used"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(INF)")), "infinitive"},
+	{regexp.MustCompile(fmt.Sprintf(NonPhoneticNotation, "Q(I GL)")), "Interviewer's Summary: %s"},
+	{regexp.MustCompile(fmt.Sprintf(NonPhoneticNotation, "Q(I)")), "interviewer's comments follow: %s"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(K)")), "known"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, `Q(\-K)`)), "unknown"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(LAT)")), "not on tape"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(LIT)")), "literary"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(MEMX)")), "informant's surpise at own recollection"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, `Q(M\/Y)`)), "response by husband or other male bystander"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(NEX)")), "did not exist"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(NN)")), "notVeryNew"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(NOUN)")), "noun"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(NP)")), "unprompted answer to prompted question"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(NT)")), "not on tape"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(OF)")), "oldfashioned"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(OOF)")), "Very Oldfashioned"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(OTW)")), "Otherwise"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(POL)")), "Informant's statement that word is not Yiddish but Polish"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(Q)")), "Check answer on tape"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(RR)")), "very rare"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(RUM)")), "Informant's statement that word is not Yiddish but Rumanian"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(RUS)")), "Informant's statement that word is not Yiddish but Russian"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(RTR)")), "rather"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(R)")), "rare"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(SMT)")), "notSometimes"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(SYN)")), "synonym"},
+	{regexp.MustCompile(fmt.Sprintf(NonPhoneticNotation, "Q(S)")), "said by: %s"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(TA)")), "tape audited"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(TF)")), "yes, fragment on tape"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(T)")), "yes, text on tape"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, `Q(\-T)`)), "text not on tape"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(UU)")), "VeryCommon"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(U)")), "Usual"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, `Q(\-U)`)), "Unusual"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(VB)")), "Verb"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(VL)")), "Vulgar"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(V)")), "Proverb"},
+	{regexp.MustCompile(fmt.Sprintf(NonPhoneticNotation, "Q(W)")), "used by: %s"},
+	{regexp.MustCompile(fmt.Sprintf(NonPhoneticNotation, `Q(\-W)`)), "not used by: %s"},
+	{regexp.MustCompile(fmt.Sprintf(NonPhoneticNotation, "Q(YID)")), "Informant's explanation in Yiddish but not necessarily verbatim or phoenetically accurate: %s"},
+	{regexp.MustCompile(fmt.Sprintf(BaseNotation, "Q(ZZ)")), "interviewer's comment: not elicitable"},
 }
 
 func Transcribe(data *dataSignal) string {
